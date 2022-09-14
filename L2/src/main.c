dd6af74bb7b1b1ddb7fc6b3df0803b0e9e87d5ff
Jams1001 <adantumora90@gmail.com>
2022-09-14 09:49:22 -0600

    Empezando con entendimiento interrupciones

--- L2/src/main.c
+++ L2/src/main.c
@@ -1,13 +1,31 @@
-#include <avr/io.h>
-#include <util/delay.h>
+#include<avr/io.h>
 
-int main(void)
-{
-  DDRB = 0x08; //Configuracion del puerto (el B3 es salida
+ISR (INT0_vect) {      // Interrupt service routine 
 
-  //Parpadear
-  while (1) {
-    PORTB = 0x00; _delay_ms(5000);
-    PORTB = 0x08; _delay_ms(5000);
+  PORTB ^= (1 << PB1);    // Toggling the PB2 pin 
+}
+
+ISR (PCINT0_vect) {       // Interrupt service routine  
+  PORTB ^= (1 << PB0);    // Toggling the PB2 pin 
+}
+
+
+void external_interrupt() {
+  DDRB |= (1<<PB1)|(1<<PB0);     // set PB2 as output(LED)
+  sei();     //enabling global interrupt
+  GIMSK |= (1 << INT0);     // enabling the INT0 (external interrupt) 
+  MCUCR |= (1 << ISC01);    // Configuring as falling edge 
+}
+
+void pin_change_interrupt() {
+  DDRB |= (1 << PB0);
+  GIMSK |= (1 << PCIE);
+  PCMSK |= (1 << PCINT1);
+}
+int main() {
+  external_interrupt();
+  pin_change_interrupt();
+  while(1) {
+    
   }
 }
