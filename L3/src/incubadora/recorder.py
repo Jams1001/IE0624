2b71ef30c4ed8afd7689ab523dc992f5cd859929
Jams1001 <adantumora90@gmail.com>
2022-10-11 18:52:55 -0600

    Se mejora la habilitación de la comunicación con la PC, se mejora el script de python

--- L3/src/incubadora/recorder.py
+++ L3/src/incubadora/recorder.py
@@ -7,13 +7,16 @@ fileName="incubadora_data.csv"
 
 ser = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port:" + arduino_port)
-file = open(fileName, "a")
+file = open(fileName, "w")
 print("Created file")
 
-line = 0
+line = 1
 sensor_data = [] 
 
+header = ['Temperatura_Termistor', 'Humedad', 'Temperatura_Operacion', 'Ctrl_sgn']
+file.write(header[0]+','+header[1]+','+header[2]+','+header[3]+'\n')
 
+print(header[0]+','+header[1]+','+header[2]+','+header[3])
 while True:
     getData=ser.readline()
     dataString = getData.decode('utf-8')
@@ -21,8 +24,9 @@ while True:
     readings = data.split(",")
     sensor_data.append(readings)
     line = line+1
+    print(data)
     
-    with open(fileName, 'w', encoding='UTF8', newline='') as f:
+    with open(fileName, 'a', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(sensor_data)
     file.close()
