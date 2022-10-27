804168bd6dae841ae68de7737682cbe4b4b2959b
Jams1001 <adantumora90@gmail.com>
2022-10-27 17:04:56 -0600

    publisher probado en el MCU

--- L4/src/gyro_publisher.py
+++ L4/src/gyro_publisher.py
@@ -2,19 +2,20 @@ import paho.mqtt.client as mqtt
 import serial, time, json
 
 
-#ser = serial.Serial(port = '/dev/ttyACM0', baudrate=115200, timeout=1) 
+ser = serial.Serial(port = '/dev/ttyACM0', baudrate=115200, timeout=1) 
 print("Connected to MCU")
 data_rows = []
 header = ['x', 'y', 'z']
 print(header)
 
-#while(1):
-#    #data = ser.readline().decode('utf-8').replace('\r', "").replace('\n', "")
-#    #data = data.split('\t')
-#
-#    if len(data) >= 3:
-#        print(data)
-data = [1, 4, 3]
+while(1):
+    data = ser.readline().decode('utf-8').replace('\r', "").replace('\n', "")
+    data = data.split('\t')
+
+    if len(data) >= 3:
+        time.sleep(2)
+        print(data)
+#data = [1, 4, 3]
 
 
 def on_connect(client, userdata, flags, rc):
@@ -32,7 +33,7 @@ def on_disconnect(client, userdata, rc):
 
 
 
-#PuertoSerial = serial.Serial(port = '/tmp/ttyACM0') 
+PuertoSerial = serial.Serial(port = '/tmp/ttyACM0') 
 print("Connected to serial port")
 client = mqtt.Client("B95222")
 client.connected = False
