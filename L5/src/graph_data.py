aad4be15037ada2f9919c83baa1bb691502ba3e8
jdgg98 <jafet.gutierrez95@gmail.com>
2022-11-16 15:06:32 -0600

    Optimización del graficador de datos

--- L5/src/graph_data.py
+++ L5/src/graph_data.py
@@ -1,29 +1,21 @@
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-filename = "punch.csv"
+filename = sys.argv[1]
 
-df = pd.read_csv("/content/" + filename)
+df = pd.read_csv(filename)
 
-index = range(1, len(df['aX']) + 1)
+index = range(1, len(df['x']) + 1)
 
 plt.rcParams["figure.figsize"] = (20,10)
 
-plt.plot(index, df['aX'], 'g.', label='x', linestyle='solid', marker=',')
-plt.plot(index, df['aY'], 'b.', label='y', linestyle='solid', marker=',')
-plt.plot(index, df['aZ'], 'r.', label='z', linestyle='solid', marker=',')
+plt.plot(index, df['x'], 'g.', label='x', linestyle='solid', marker=',')
+plt.plot(index, df['y'], 'b.', label='y', linestyle='solid', marker=',')
+plt.plot(index, df['z'], 'r.', label='z', linestyle='solid', marker=',')
 plt.title("Acceleration")
 plt.xlabel("Sample #")
 plt.ylabel("Acceleration (G)")
-plt.legend()
-plt.show()
-
-plt.plot(index, df['gX'], 'g.', label='x', linestyle='solid', marker=',')
-plt.plot(index, df['gY'], 'b.', label='y', linestyle='solid', marker=',')
-plt.plot(index, df['gZ'], 'r.', label='z', linestyle='solid', marker=',')
-plt.title("Gyroscope")
-plt.xlabel("Sample #")
-plt.ylabel("Gyroscope (deg/sec)")
 plt.legend()
 plt.show()
