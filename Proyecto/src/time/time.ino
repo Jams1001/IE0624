c0180e5e21a70be9e39d4491e1f71d6b6cc5fd44
Jams1001 <adantumora90@gmail.com>
2022-12-04 12:06:33 -0600

    Firmware para llevar control de tiempo

---
+++ Proyecto/src/time/time.ino
@@ -0,0 +1,35 @@
+#include <TimeLib.h>
+
+void setup()  {
+  Serial.begin(9600);
+  setTime(1670392800);
+}
+
+void loop(){    
+  digitalClockDisplay();  
+  delay(1000);
+}
+
+void digitalClockDisplay(){
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+
+  
