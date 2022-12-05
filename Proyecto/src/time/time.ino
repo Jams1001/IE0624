417d1a7120c1a28b97cab05983cca0d6acc9cdb2
Jams1001 <adantumora90@gmail.com>
2022-12-04 20:33:14 -0600

    time firmware version 2

--- Proyecto/src/time/time.ino
+++ Proyecto/src/time/time.ino
@@ -1,36 +1,58 @@
-#include <TimeLib.h>
+#include <TimerOne.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
 
-void setup()  {
-  Serial.begin(9600);
-  //setTime(1670178144);
-  setTime(Serial.readString());
+const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 13;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int horas=6;
+int minutos=0;
+volatile int segundos=0;
+volatile boolean actualizar=true;
+
+void setup(){
+  Wire.begin();  
+  lcd.begin(16,2);
+
+  Timer1.initialize(1000000);
+  Timer1.attachInterrupt(manejadoraTimer);
 }
 
-void loop(){    
-  digitalClockDisplay();  
-  delay(1000);
+void manejadoraTimer(){
+    segundos++;
+    actualizar=true;
 }
 
-void digitalClockDisplay(){
-  Serial.print(hour());
-  printDigits(minute());
-  printDigits(second());
-  Serial.print(" ");
-  Serial.print(day());
-  Serial.print(" ");
-  Serial.print(month());
-  Serial.print(" ");
-  Serial.print(year()); 
-  Serial.println(); 
+void actualizarReloj(){
+  minutos += segundos / 60;
+  segundos = segundos % 60;
+
+  horas += minutos / 60;
+  minutos = minutos % 60;
+
+  horas = horas % 24;
 }
 
-void printDigits(int digits){
-  // utility function for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
-
-
+void loop(){
+  if(actualizar == true){
+    actualizarReloj();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    if(horas < 10){
+      lcd.print("0");
+    }
+    lcd.print(horas);
+    lcd.print(":");
+    if(minutos < 10){
+      lcd.print("0");
+    }
+    lcd.print(minutos);
+    lcd.print(":");
+    if(segundos < 10){
+      lcd.print("0");
+    }
+    lcd.print(segundos);
+    actualizar = false;
+  }
   
+}
