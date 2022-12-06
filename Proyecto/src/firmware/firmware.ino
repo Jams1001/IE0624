4e769ac7e4fd56034911f71e7988c14f426cc063
jdgg98 <jafet.gutierrez95@gmail.com>
2022-12-06 02:41:04 -0600

    Actualizando firmware

--- Proyecto/src/firmware/firmware.ino
+++ Proyecto/src/firmware/firmware.ino
@@ -1,16 +1,65 @@
-#define LED_PIN 11
-#define POTENTIOMETER_PIN A1
+#include <TimerOne.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <RBDdimmer.h>
 
-String pos, prev_pos;
+#define outputPin  12 // PSM
+#define zerocross  2
+// #define relayPin 4
 
-void setup()
-{
-  Serial.begin(9600); 
-  pinMode(LED_PIN, OUTPUT);
+String pos = "0";
+String prev_pos = "0";
+long segundos_t;
+long horas;
+long minutos;
+volatile long segundos;
+volatile boolean actualizar = true;
+
+const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 13;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+dimmerLamp dimmer(outputPin);
+
+void setup(){
+  Serial.begin(9600);
+  Wire.begin();
+
+  // pinMode(relayPin, OUTPUT);
+
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.print("Waiting...");
+
+  while(!Serial.available()){}
+
+  segundos_t = Serial.parseInt();
+
+  horas = segundos_t/3600;
+  minutos = segundos_t/60 - horas*60;
+  segundos = segundos_t - horas*3600 - minutos*60;
+  
+  dimmer.begin(NORMAL_MODE, ON);
+
+  Timer1.initialize(1000000);
+  Timer1.attachInterrupt(manejadoraTimer);
 }
 
-void loop()
-{
+void manejadoraTimer(){
+    segundos++;
+    actualizar=true;
+}
+
+void actualizarReloj(){
+  minutos += segundos / 60;
+  segundos = segundos % 60;
+
+  horas += minutos / 60;
+  minutos = minutos % 60;
+
+  horas = horas % 24;
+}
+
+void loop(){
   if(Serial.available()){
     pos = Serial.readString();
     prev_pos = pos;
@@ -19,43 +68,103 @@ void loop()
     pos = prev_pos;
   }
 
+  if((horas == 7) && (minutos == 0) && (segundos == 0)){
+    pos = 40;
+    prev_pos = pos;
+  }
+
+  if((horas == 8) && (minutos == 30) && (segundos == 0)){
+    pos = 80;
+    prev_pos = pos;
+  }
+
+  if((horas == 20) && (minutos == 45) && (segundos == 0)){
+    pos = 45;
+    prev_pos = pos;
+  }
+
+  if((horas == 22) && (minutos == 15) && (segundos == 0)){
+    pos = 20;
+    prev_pos = pos;
+  }
+
   if (pos == "0"){
-    analogWrite(LED_PIN, 0);
+    dimmer.setPower(20);
   }
 
   else if (pos == "10"){
-    analogWrite(LED_PIN, 25);
+    dimmer.setPower(25);
   }
 
   else if (pos == "20"){
-    analogWrite(LED_PIN, 51);
+    dimmer.setPower(27);
   }
 
   else if (pos == "30"){
-    analogWrite(LED_PIN, 77);
+    dimmer.setPower(35);
   }
 
   else if (pos == "40"){
-    analogWrite(LED_PIN, 102);
+    dimmer.setPower(37);
   }
 
   else if (pos == "50"){
-    analogWrite(LED_PIN, 128);
+    dimmer.setPower(40);
   }
 
   else if (pos == "60"){
-    analogWrite(LED_PIN, 153);
+    dimmer.setPower(43);
   }
 
   else if (pos == "70"){
-    analogWrite(LED_PIN, 179);
+    dimmer.setPower(50);
   }
 
   else if (pos == "80"){
-    analogWrite(LED_PIN, 204);
+    dimmer.setPower(56);
   }
   
   else if (pos == "90"){
-    analogWrite(LED_PIN, 230);
+    dimmer.setPower(67);
+  }
+  
+  if(actualizar == true){
+    actualizarReloj();
+    
+    lcd.clear();
+    lcd.setCursor(0,0);
+    
+    if(horas < 10){
+      lcd.print("0");
+    }
+    lcd.print(horas);
+    
+    lcd.print(":");
+    
+    if(minutos < 10){
+      lcd.print("0");
+    }
+    lcd.print(minutos);
+    
+    lcd.print(":");
+    
+    if(segundos < 10){
+      lcd.print("0");
+    }
+    lcd.print(segundos);    
+
+    if(pos == "0"){
+      lcd.setCursor(14,1);
+    }
+    else{
+      lcd.setCursor(13,1);
+    }
+
+    lcd.print(pos);
+
+    lcd.setCursor(15,1);
+    lcd.print("%");
+    
+    actualizar = false;
   }
 }
